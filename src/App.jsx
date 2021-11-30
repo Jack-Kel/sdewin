d38b3136fed0cc2f7e35273efdba8325f326d393
Jack Kelly <jackkelly12345@gmail.com>
2021-11-30 19:03:48 +1100

    Tailwind and minor styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Children } from 'react'
 import './App.css'
 
 var lastUpdate = performance.now()
 
 function App() {
-  const [gold, setGold] = useState(0)
+  const [gold, setGold] = useState(500)
   const [crops, setCrops] = useState({})
 
   const growParsnips = () => {
-    setCrops((crops) => {
-      crops["parsnips"] = 2000
-      return crops
-    })
+    if(gold > 20){
+      setGold((gold) => gold - 20)
+
+      setCrops((crops) => {
+        crops["parsnips"] = 4000
+        return crops
+      })
+    }
   }
   //https://isaacsukin.com/news/2015/01/detailed-explanation-javascript-game-loops-and-timing
   // if you want to impove game loop/update time
@@ -26,7 +30,7 @@ function App() {
       return crops
     })
     if (crops["parsnips"] <= 0){
-      setGold((gold) => gold + 30)
+      setGold((gold) => gold + 35)
       setCrops((crops) => {
         delete crops["parsnips"]
         return crops
@@ -41,18 +45,22 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <button type="button" onClick={() => setGold((gold) => gold + 1)}>
-            gold is: {gold}
-          </button>
-          <button type="button" onClick={growParsnips}>
-            grow parsnips!
-          </button>
-        </p>
-      </header>
+    <div className="container mx-auto p-4 flex flex-wrap gap-4">
+      <Button onClick={() => setGold((gold) => gold + 1)}>
+        gold is: {gold}
+      </Button>
+      <Button onClick={growParsnips}>
+        grow parsnips!
+      </Button>
     </div>
+  )
+}
+
+const Button = ({children, ...props}) => {
+  return (
+    <button className="bg-blue-300 px-4 py-2 text-2xl font-semibold text-blue-700" type="button" {...props}>
+      {children}
+    </button>
   )
 }
 
